66beae7dc8fa5e153cf8d5fecf52dfcbdfe3420f
VincenzoAmari <vincenzo.am99@gmail.com>
2025-07-14 11:21:59 +0200

    update mediaquery

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { useFavorites } from "../context/FavoritesContext";
 import { FaSearch } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = ({ search, setSearch }) => {
-  const { favorites } = useFavorites();
   const location = useLocation();
 
   const showSearch =
